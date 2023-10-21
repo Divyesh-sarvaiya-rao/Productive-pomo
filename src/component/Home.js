80c974563b509fe0441711c26830b8d949874316
TNRjain <jainishkhandhar.rao@gmail.com>
2023-10-21 12:18:00 +0530

    gh-pages

---
+++ src/component/Home.js
@@ -0,0 +1,183 @@
+import { useState, useEffect, useRef } from 'react'
+import Timer from './Timer.js'
+import Task from './Task.js'
+
+function Home(props) {
+  console.log('home component Time :',props.taskTime);
+  let pomodoroTime = 25 * 60;
+  let shortbreacktime = 5 * 60;
+  let longbreacktime = 15 *60;
+ 
+  // console.log('homeTime::',pomodoroTime);
+  let mainTime = {
+    pomodoro: pomodoroTime,
+    shortBreak: shortbreacktime,
+    longBreak: longbreacktime
+  }
+  const bgcolorType = {
+    color: 'rgb(186, 73, 73)',
+    type: 'mainClock'
+  }
+  const [bgcolor, setColor] = useState(bgcolorType)
+  // const [btnColor,setBtnColor] = useState('rgba(0, 0, 0, 0.15)')
+  const backgroundChange = (color, type) => {
+    setColor(color = { color: color, type: type });
+  }
+  useEffect(() => {
+    document.body.style.backgroundColor = bgcolor.color;
+  })
+  const [activebutton, setactivebutton] = useState('first');
+  const clickedbuttonhandler = (name) => {
+    setactivebutton(name);
+    setStart(false)
+    console.log(activebutton);
+  }; 
+  let [timer, setTimer] = useState(mainTime);
+  const [start, setStart] = useState(false);
+  const firstStart = useRef(true);
+  const tick = useRef();
+
+  // if (props && props.taskTime) {
+  //   pomodoroTime=props.taskTime.pomodoroTime;
+  //   shortbreacktime=props.taskTime.shortBreakTime;
+  //   longbreacktime=props.taskTime.longbreacktime;
+  //   const mainTime = {
+  //     pomodoro: pomodoroTime * 60,
+  //     shortBreak: shortbreacktime * 60,
+  //     longBreak: longbreacktime * 60
+  //   }
+  //   setTimer(mainTime)
+  // }
+  useEffect(() => {
+    if (firstStart.current) {
+      console.log("first render, don't run useEffect for timer");
+      firstStart.current = !firstStart.current;
+      return;
+    }
+    if (start) {
+      let time;
+      tick.current = setInterval(() => {
+        if (bgcolor.type === 'mainClock') {
+        if (timer.pomodoro === 0) {
+          console.log('timer component clock out');
+          clearInterval(tick.current);
+          setStart(false);
+          if(window.confirm("Your task is completed Do you want to Reset this clock ")){
+          time = { pomodoro: pomodoroTime, shortBreak: shortbreacktime, longBreak: longbreacktime }
+          }
+        } else {
+          time = { pomodoro: timer.pomodoro - 1, shortBreak: shortbreacktime, longBreak: longbreacktime }
+        }
+        console.log("mainClock called : ", time);
+      }
+      else if (bgcolor.type === 'shortClock') {
+        if (timer.shortBreak === 0) {
+          console.log('timer component clock out');
+          clearInterval(tick.current);
+          setStart(false);
+          backgroundChange("rgb(186, 73, 73)", "mainClock");
+          pomodorotime();
+          clickedbuttonhandler('first');
+          time = { pomodoro: timer.pomodoro, shortBreak: shortbreacktime, longBreak: longbreacktime }
+        } else {
+          time = { pomodoro: timer.pomodoro, shortBreak: timer.shortBreak - 1, longBreak: longbreacktime }
+        }
+      }
+      else if (bgcolor.type === 'longClock') {
+        if (timer.longBreak === 0) {
+          console.log('timer component clock out');
+          clearInterval(tick.current);                               
+          setStart(false);
+          backgroundChange("rgb(186, 73, 73)", "mainClock");
+          pomodorotime();
+          clickedbuttonhandler('first');
+          time = { pomodoro: timer.pomodoro, shortBreak: shortbreacktime, longBreak: longbreacktime }
+        } else {
+          time = { pomodoro: timer.pomodoro, shortBreak: shortbreacktime, longBreak: timer.longBreak - 1 }
+        }
+      }
+        setTimer(() => timer = time);
+      }, 1000);
+    } else {
+      console.log("clear interval");
+      clearInterval(tick.current);
+    }
+    return () => clearInterval(tick.current);
+  },[start]);
+
+  const toggleStart = () => {
+    setStart(!start);
+  };
+
+  const dispSecondsAsMins = (seconds) => {
+    console.log("seconds :" + seconds);
+    const mins = Math.floor(seconds / 60);
+    const seconds_ = seconds % 60;
+    return mins.toString() + ":" + (seconds_ === 0 ? "00" : seconds_.toString());
+  };
+  const pomodorotime = () => {
+    console.log("pomodoro function called :")
+    // setTimer(time);  
+  }
+  const shortBreack = () => {
+    console.log("shortBreack function called : ")
+    // setTimer(shortbreacktime);
+  }
+  const longBreack = () => {
+    console.log("longBreack function called : ")
+    // setTimer(longbreacktime);
+  }
+  let taskName;
+    if (bgcolor.type==='mainClock') {
+      taskName='Time to focus!'
+    } else if(bgcolor.type==='shortClock'){
+      taskName='Time to Shortbreak!'
+    }
+    else if (bgcolor.type==='longClock') {
+      taskName='Time to Longbreak!'
+    }
+    
+  return (
+    <>
+      <div className='home_component' style={{ backgroundColor: bgcolor.color }}>
+
+        <div className='first_row'>
+
+          <div className='row second_row'>
+
+            <div className=' button_col'>
+
+              <button id='pomodoro' onClick={() => { backgroundChange("rgb(186, 73, 73)", "mainClock"); pomodorotime();clickedbuttonhandler('first') }}
+              style={{backgroundColor: activebutton==='first'? 'rgba(0, 0, 0, 0.15)':''}} >Pomodoro</button>
+              <button id='shortBreack' onClick={() => { backgroundChange("rgb(56,133,138)", "shortClock"); shortBreack();clickedbuttonhandler('second') }}
+              style={{backgroundColor: activebutton==='second'? 'rgba(0, 0, 0, 0.15)':''}}>Short Break</button>
+              <button id='longBreack' onClick={() => { backgroundChange("rgb(57,112,151)", "longClock"); longBreack();clickedbuttonhandler('third') }}
+              style={{backgroundColor: activebutton==='third'? 'rgba(0, 0, 0, 0.15)':''}} >Long Break</button>
+            </div>
+            <div className='timer-div '>
+
+              <Timer dispSecondsAsMins={dispSecondsAsMins} timer={timer} bgcolor={bgcolor.type} />
+
+              <div className="startDiv text-center">
+
+                <button className="startBut" onClick={toggleStart} style={{ color: bgcolor.color }}>
+                  {!start ? "START" : "STOP"}
+                </button>
+
+              </div>
+
+            </div>
+
+          </div>
+
+          <div className='pomodoro_count text-center'>#1</div>
+        <div className='taskName text-center'>{taskName}</div>
+          <Task />
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home
---
+++ src/component/Task.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react'
+import { FaBars } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
+import { FaCheckCircle } from 'react-icons/fa'
+
+function Task() {
+	const [visible, setVisible] = useState(false);
+	const close_taskaddbox = () => {
+		setVisible(false)
+	}
+	const initial = { taskName: '', taskTime: '' }
+	let data = localStorage.getItem('added_task')
+	let array;
+	if (data) {
+		array = JSON.parse(data);
+	}
+	else {
+		array = [];
+	}
+	const [check, setCheck] = useState(false);
+	const [inputText, setInputText] = useState(initial);
+	const [task, setTask] = useState(array);
+	const submit = () => {
+		console.log("Task added");
+		if (inputText.taskName === '') {
+			alert('Please something to add task')
+		} else {
+			let inputTask = {
+				taskId: new Date().getTime().toString(),
+				taskName: inputText.taskName,
+				taskTime: inputText.taskTime,
+			}
+			setTask([...task, inputTask])
+			setInputText(initial);
+			task.push(inputTask)
+			localStorage.setItem('added_task', JSON.stringify(task));
+		}
+	}
+	const deleteTask = (id) => {
+		let choice = window.confirm("Do you want Delete this Task ");
+		if (choice) {
+			let newTask = task.filter((tasklist)=> tasklist.taskId !== id);
+			// newTask.splice(id, 1)
+			setTask([...newTask]);
+			console.log("Funcion deleted with this ID :", id);
+			localStorage.setItem('added_task', JSON.stringify(newTask));
+		}
+	}
+	const check_task = () => {
+		console.log("check task called ::");
+		setCheck(true);
+	}
+	return (
+		<div className='task_component'>
+			<div className='task_header'>
+				<span>Tasks</span>
+				<div className='dots_menu'>
+					<FaBars />
+				</div>
+			</div>
+			{
+				task.map((taskList, i) => {
+					return (<div className='task_preview' key={i} index={i}>
+						<div id='taskName' style={{ textDecoration: check ? 'line-through' : '' }}>{taskList.taskName}</div>
+						<div className='check_task' onClick={() => { check_task() }}
+							style={{ color: check ? 'green' : 'white' }}><FaCheckCircle /></div>
+						<div className='delete_task' onClick={() => { deleteTask(taskList.taskId) }}><FaTrash /></div>
+					</div>)
+				})
+			}
+			{!visible && <div className='task_button' onClick={() => setVisible(!visible)}>
+				<strong>+</strong> Add Task
+			</div>}
+
+
+			{visible && <div className='task_add_box'>
+				<input type='text' className='task_name_input' placeholder='What are you working on?' value={inputText.taskName}
+					onChange={e => { setInputText({ taskName: e.target.value, taskTime: inputText.taskTime }) }} />
+				<span>Est Pomodoros</span><br />
+				<input type='number' className='number_input' min='0' step='1' placeholder="Est Time" value={inputText.taskTime}
+					onChange={e => { setInputText({ taskName: inputText.taskName, taskTime: e.target.value }) }} />
+				<div className='btn_div'>
+					<button className='cancel_btn' onClick={close_taskaddbox}>Cancel</button>
+					<button className='save_btn' onClick={() => { submit(); close_taskaddbox() }}>Save</button>
+				</div>
+			</div>}
+
+		</div>
+	)
+}
+
+export default Task
