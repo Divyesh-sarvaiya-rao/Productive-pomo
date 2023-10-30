5ac47f616b73d0cd8fdad58409cd7305fda3afaa
Divyesh-sarvaiya-rao <divyeshsarvaiya.rao@gmail.com>
2023-10-30 19:42:18 +0530

    update

--- src/component/Home.js
+++ src/component/Home.js
@@ -8,8 +8,8 @@ import sound from '../sounds/music2.wav';
 function Home() {
 
   let pomodoroTime = 25 *60;
-  let shortbreacktime = 5 *60;
-  let longbreacktime =  15*60;
+  let shortbreackTime = 5 *60;
+  let longbreackTime =  15*60;
   
   let locaSetTime = localStorage.getItem('userSetTime');
   let mainTime;
@@ -19,8 +19,8 @@ function Home() {
   else{
     mainTime = {
       pomodoro: pomodoroTime,
-      shortBreak: shortbreacktime,
-      longBreak: longbreacktime
+      shortBreak: shortbreackTime,
+      longBreak: longbreackTime
     }
   }
 
@@ -35,6 +35,7 @@ function Home() {
   }
   useEffect(() => {
     document.body.style.backgroundColor = bgcolor.color;
+    document.body.style.transition = 'background-color 1s'
   })
   const [activebutton, setactivebutton] = useState('first');
   const clickedbuttonhandler = (name) => {
@@ -198,25 +199,12 @@ const reset_inputs=()=>{
 					</div>
 			</h4>
 			<div className='header-button'>
-				{/* <button>
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" className="bi bi-bar-chart" viewBox="0 0 16 16">
- 					<path d="M4 11H2v3h2v-3zm5-4H7v7h2V7zm5-5v12h-2V2h2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3z"/>
-					</svg>
-					<span className='header_btn'>&emsp;Report</span>
-				</button> */}
 				<button onClick={() => setVisible(!visible)}>
 					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-gear-wide" viewBox="0 0 16 16">
   					<path d="M8.932.727c-.243-.97-1.62-.97-1.864 0l-.071.286a.96.96 0 0 1-1.622.434l-.205-.211c-.695-.719-1.888-.03-1.613.931l.08.284a.96.96 0 0 1-1.186 1.187l-.284-.081c-.96-.275-1.65.918-.931 1.613l.211.205a.96.96 0 0 1-.434 1.622l-.286.071c-.97.243-.97 1.62 0 1.864l.286.071a.96.96 0 0 1 .434 1.622l-.211.205c-.719.695-.03 1.888.931 1.613l.284-.08a.96.96 0 0 1 1.187 1.187l-.081.283c-.275.96.918 1.65 1.613.931l.205-.211a.96.96 0 0 1 1.622.434l.071.286c.243.97 1.62.97 1.864 0l.071-.286a.96.96 0 0 1 1.622-.434l.205.211c.695.719 1.888.03 1.613-.931l-.08-.284a.96.96 0 0 1 1.187-1.187l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205a.96.96 0 0 1 .434-1.622l.286-.071c.97-.243.97-1.62 0-1.864l-.286-.071a.96.96 0 0 1-.434-1.622l.211-.205c.719-.695.03-1.888-.931-1.613l-.284.08a.96.96 0 0 1-1.187-1.186l.081-.284c.275-.96-.918-1.65-1.613-.931l-.205.211a.96.96 0 0 1-1.622-.434L8.932.727zM8 12.997a4.998 4.998 0 1 1 0-9.995 4.998 4.998 0 0 1 0 9.996z"/>
 					</svg>
 					<span className='header_btn'>&emsp;Setting</span> 
 				</button>
-				{/* <button>
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-person-circle" viewBox="0 0 16 16">
-  					<path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-  					<path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-					</svg>
-					<span className='header_btn'>&emsp;Login</span>
-				</button> */}
 			</div>
 		</div>
 		{visible && <div className='setting_div'>
@@ -247,7 +235,7 @@ const reset_inputs=()=>{
 		</div>}
     {/* Home component start here */}
 
-    { !visible&& <div className='home_component' style={{ backgroundColor: bgcolor.color }}>
+    { !visible&& <div className='home_component' >
     
         <div className='first_row'>
 
@@ -268,7 +256,7 @@ const reset_inputs=()=>{
 
               <div className="startDiv text-center">
 
-                <button className="startBut" onClick={toggleStart} style={{ color: bgcolor.color }}>
+                <button className="startBut" onClick={toggleStart} style={{ color: bgcolor.color,transition:'color 2s'}}>
                   {!start ? "START" : "STOP"}
                 </button>
 
