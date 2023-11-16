a8a53503e5f7d26f09812255e460457a07f82cb3
Divyesh Sarvaiya <145099438+Divyesh-sarvaiya-rao@users.noreply.github.com>
2023-11-16 19:41:55 +0530

    Update

--- src/component/Home.js
+++ src/component/Home.js
@@ -35,7 +35,7 @@ function Home() {
   }
   useEffect(() => {
     document.body.style.backgroundColor = bgcolor.color;
-    document.body.style.transition = 'background-color 1s'
+    document.body.style.transition = 'background-color 3s'
   })
   const [activebutton, setactivebutton] = useState('first');
   const clickedbuttonhandler = (name) => {
@@ -192,84 +192,72 @@ const reset_inputs=()=>{
   return (
     <>
     {/* header component start here */}
-    <div className='header'>
-			<h4 className='header-text'>
-					<div>
-						<FaCheckCircle />&nbsp;Productive Pomo
-					</div>
-			</h4>
-			<div className='header-button'>
-				<button onClick={() => setVisible(!visible)}>
-					<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-gear-wide" viewBox="0 0 16 16">
-  					<path d="M8.932.727c-.243-.97-1.62-.97-1.864 0l-.071.286a.96.96 0 0 1-1.622.434l-.205-.211c-.695-.719-1.888-.03-1.613.931l.08.284a.96.96 0 0 1-1.186 1.187l-.284-.081c-.96-.275-1.65.918-.931 1.613l.211.205a.96.96 0 0 1-.434 1.622l-.286.071c-.97.243-.97 1.62 0 1.864l.286.071a.96.96 0 0 1 .434 1.622l-.211.205c-.719.695-.03 1.888.931 1.613l.284-.08a.96.96 0 0 1 1.187 1.187l-.081.283c-.275.96.918 1.65 1.613.931l.205-.211a.96.96 0 0 1 1.622.434l.071.286c.243.97 1.62.97 1.864 0l.071-.286a.96.96 0 0 1 1.622-.434l.205.211c.695.719 1.888.03 1.613-.931l-.08-.284a.96.96 0 0 1 1.187-1.187l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205a.96.96 0 0 1 .434-1.622l.286-.071c.97-.243.97-1.62 0-1.864l-.286-.071a.96.96 0 0 1-.434-1.622l.211-.205c.719-.695.03-1.888-.931-1.613l-.284.08a.96.96 0 0 1-1.187-1.186l.081-.284c.275-.96-.918-1.65-1.613-.931l-.205.211a.96.96 0 0 1-1.622-.434L8.932.727zM8 12.997a4.998 4.998 0 1 1 0-9.995 4.998 4.998 0 0 1 0 9.996z"/>
-					</svg>
-					<span className='header_btn'>&emsp;Setting</span> 
-				</button>
+	<div className='header'>
+		<h4 className='header-text'>
+			<div>
+				<FaCheckCircle />&nbsp;Productive Pomo
+			</div>
+		</h4>
+		<div className='header-button'>
+			<button onClick={() => setVisible(!visible)}>
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-gear-wide" viewBox="0 0 16 16">
+				<path d="M8.932.727c-.243-.97-1.62-.97-1.864 0l-.071.286a.96.96 0 0 1-1.622.434l-.205-.211c-.695-.719-1.888-.03-1.613.931l.08.284a.96.96 0 0 1-1.186 1.187l-.284-.081c-.96-.275-1.65.918-.931 1.613l.211.205a.96.96 0 0 1-.434 1.622l-.286.071c-.97.243-.97 1.62 0 1.864l.286.071a.96.96 0 0 1 .434 1.622l-.211.205c-.719.695-.03 1.888.931 1.613l.284-.08a.96.96 0 0 1 1.187 1.187l-.081.283c-.275.96.918 1.65 1.613.931l.205-.211a.96.96 0 0 1 1.622.434l.071.286c.243.97 1.62.97 1.864 0l.071-.286a.96.96 0 0 1 1.622-.434l.205.211c.695.719 1.888.03 1.613-.931l-.08-.284a.96.96 0 0 1 1.187-1.187l.283.081c.96.275 1.65-.918.931-1.613l-.211-.205a.96.96 0 0 1 .434-1.622l.286-.071c.97-.243.97-1.62 0-1.864l-.286-.071a.96.96 0 0 1-.434-1.622l.211-.205c.719-.695.03-1.888-.931-1.613l-.284.08a.96.96 0 0 1-1.187-1.186l.081-.284c.275-.96-.918-1.65-1.613-.931l-.205.211a.96.96 0 0 1-1.622-.434L8.932.727zM8 12.997a4.998 4.998 0 1 1 0-9.995 4.998 4.998 0 0 1 0 9.996z"/>
+				</svg>
+				<span className='header_btn'>&emsp;Setting</span> 
+			</button>
+		</div>
+	</div>
+	{visible && <div className='setting_div'>
+		<div className="container">
+			<div className=" timeName"><span>Time (minutes)</span></div>
+			<div className=" time_input_div">
+				<div className=" input_div">
+					<label className=" lable_name">Pomodoro</label>
+					<input type="number" min="0" step="1" className="number_input" value={inputTime.pomodoro} 
+					onChange={e=>{setInputTime({pomodoro:e.target.value,shortBreak:inputTime.shortBreak,longBreak:inputTime.longBreak})}}/>
+				</div>
+				<div className=" input_div">
+					<label className=" lable_name">Short Break</label>
+					<input type="number" min="0" step="1" className="number_input" value={inputTime.shortBreak} 
+					onChange={e=>{setInputTime({pomodoro:inputTime.pomodoro,shortBreak:e.target.value,longBreak:inputTime.longBreak})}}/>
+				</div>
+				<div className=" input_div">
+					<label className=" lable_name">Long Break</label>
+					<input type="number" min="0" step="1" className="number_input" value={inputTime.longBreak} 
+					onChange={e=>{setInputTime({pomodoro:inputTime.pomodoro,shortBreak:inputTime.shortBreak,longBreak:e.target.value})}}/>
+				</div>
+			</div>
+			<div className='setting_done_btn'>
+				<button className='reset_btn' onClick={reset_inputs}>Reset</button>
+				<button className='done_btn ' onClick={setting_done} >Done</button>
 			</div>
 		</div>
-		{visible && <div className='setting_div'>
-				<div className="container">
-					<div className=" timeName"><span>Time (minutes)</span></div>
-					<div className=" time_input_div">
-						<div className=" input_div">
-							<label className=" lable_name">Pomodoro</label>
-							<input type="number" min="0" step="1" className="number_input" value={inputTime.pomodoro} 
-							onChange={e=>{setInputTime({pomodoro:e.target.value,shortBreak:inputTime.shortBreak,longBreak:inputTime.longBreak})}}/>
-						</div>
-						<div className=" input_div">
-							<label className=" lable_name">Short Break</label>
-							<input type="number" min="0" step="1" className="number_input" value={inputTime.shortBreak} 
-							onChange={e=>{setInputTime({pomodoro:inputTime.pomodoro,shortBreak:e.target.value,longBreak:inputTime.longBreak})}}/>
-						</div>
-						<div className=" input_div">
-							<label className=" lable_name">Long Break</label>
-							<input type="number" min="0" step="1" className="number_input" value={inputTime.longBreak} 
-							onChange={e=>{setInputTime({pomodoro:inputTime.pomodoro,shortBreak:inputTime.shortBreak,longBreak:e.target.value})}}/>
-						</div>
-					</div>
-					<div className='setting_done_btn'>
-            <button className='reset_btn' onClick={reset_inputs}>Reset</button>
-						<button className='done_btn ' onClick={setting_done} >Done</button>
-					</div>
-				</div>
-		</div>}
+	</div>}
     {/* Home component start here */}
 
     { !visible&& <div className='home_component' >
-    
         <div className='first_row'>
-
           <div className='row second_row'>
-
-            <div className=' button_col'>
-
-              <button id='pomodoro' onClick={() => { backgroundChange("rgb(186, 73, 73)", "mainClock"); pomodorotime();clickedbuttonhandler('first') }}
-              style={{backgroundColor: activebutton==='first'? 'rgba(0, 0, 0, 0.15)':''}} >Pomodoro</button>
-              <button id='shortBreack' onClick={() => { backgroundChange("rgb(56,133,138)", "shortClock"); shortBreack();clickedbuttonhandler('second') }}
-              style={{backgroundColor: activebutton==='second'? 'rgba(0, 0, 0, 0.15)':''}}>Short Break</button>
-              <button id='longBreack' onClick={() => { backgroundChange("rgb(57,112,151)", "longClock"); longBreack();clickedbuttonhandler('third') }}
-              style={{backgroundColor: activebutton==='third'? 'rgba(0, 0, 0, 0.15)':''}} >Long Break</button>
-            </div>
+		<div className=' button_col'>
+			<button id='pomodoro' onClick={() => { backgroundChange("rgb(186, 73, 73)", "mainClock"); pomodorotime();clickedbuttonhandler('first') }}
+			style={{backgroundColor: activebutton==='first'? 'rgba(0, 0, 0, 0.15)':''}} >Pomodoro</button>
+			<button id='shortBreack' onClick={() => { backgroundChange("rgb(56,133,138)", "shortClock"); shortBreack();clickedbuttonhandler('second') }}
+			style={{backgroundColor: activebutton==='second'? 'rgba(0, 0, 0, 0.15)':''}}>Short Break</button>
+			<button id='longBreack' onClick={() => { backgroundChange("rgb(57,112,151)", "longClock"); longBreack();clickedbuttonhandler('third') }}
+			style={{backgroundColor: activebutton==='third'? 'rgba(0, 0, 0, 0.15)':''}} >Long Break</button>
+		</div>
             <div className='timer-div '>
-
               <Timer dispSecondsAsMins={dispSecondsAsMins} timer={timer} bgcolor={bgcolor.type} />
-
               <div className="startDiv text-center">
-
-                <button className="startBut" onClick={toggleStart} style={{ color: bgcolor.color,transition:'color 2s'}}>
+                <button className="startBut" onClick={toggleStart} style={{ color: bgcolor.color,transition:'color 3s'}}>
                   {!start ? "START" : "STOP"}
                 </button>
-
               </div>
-
             </div>
-
           </div>
-
           {/* <div className='pomodoro_count text-center'>#1</div> */}
         <div className='taskName text-center'>{taskName}</div>
           <Task />
-
         </div>
       </div>}
     </>
